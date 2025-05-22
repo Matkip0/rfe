3cc6b948661e7eb445b9b8d66eaa9fa08343b524
Matkip0 <biegel987@gmail.com>
2025-05-22 12:15:43 +0200

    fix(file system): add conditional 'use' in read_directory depending on whether it is running on unix or windows

--- src-tauri/src/rfe_directory/read_directory.rs
+++ src-tauri/src/rfe_directory/read_directory.rs
@@ -2,11 +2,15 @@ use std::string::String;
 use std::fs;
 use std::ffi::OsStr;
 use std::fs::{read_dir, DirEntry, Metadata};
-use std::os::unix::fs::MetadataExt;
 use std::path::{Path, PathBuf};
 use chrono::{DateTime, Local};
 use anyhow::{anyhow, Result};
 use serde::{Deserialize, Serialize};
+
+#[cfg(any(target_os = "linux", target_os = "macos"))]
+use std::os::unix::fs::MetadataExt;
+#[cfg(target_os = "windows")]
+use std::os::windows::fs::MetadataExt;
 
 #[derive(Serialize, Deserialize)]
 pub enum ItemType{
@@ -42,7 +46,7 @@ pub fn read_directory(currentPath: &str) -> Result<Vec<Item>, &'static str> {
 
     for entry in paths{
         let item = read_dir_entry(entry.unwrap());
-        match item { 
+        match item {
             Ok(prop) => items.push(prop),
             Err(_) => continue,
         }
@@ -61,7 +65,7 @@ fn read_dir_entry(entry: DirEntry) -> Result<Item>{
         Some(prop) => prop,
         None => return Err(anyhow!("Error converting to str"))
     };
-    
+
     let item_name_as_string = String::from(item_name_as_str);
     let item_type = find_item_type(&entry_path, &entry_metadata).unwrap();
     let modified_date: DateTime<Local> = entry_metadata.modified()?.into();
@@ -84,10 +88,13 @@ fn find_item_type(entry_path: &PathBuf, entry_metadata: &Metadata) -> Result<Ite
     if entry_path.is_file(){
         item_type = ItemType::File(File{
             file_type: String::from(entry_path.extension().and_then(OsStr::to_str).unwrap_or("")),
+            #[cfg(any(target_os = "linux", target_os = "macos"))]
             size_in_bytes: entry_metadata.size(),
+            #[cfg(target_os = "windows")]
+            size_in_bytes: entry_metadata.file_size(),
         });
         return  Ok(item_type);
-        
+
     } else if entry_path.is_dir() {
         item_type = ItemType::Directory(Directory{
             item_count: match read_dir(entry_path){
@@ -97,6 +104,6 @@ fn find_item_type(entry_path: &PathBuf, entry_metadata: &Metadata) -> Result<Ite
         });
         return  Ok(item_type);
     }
-    
+
     Err("item could not be validated")
 }
